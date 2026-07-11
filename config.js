5b0718b9efbbca61836ddbf69077a1a8aca5bd77
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2026-07-11 16:33:30 -0500

    Add config.js to manage environment variables

---
+++ config.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+
+BOT_TOKEN: process.env.BOT_TOKEN,
+
+OWNER_ID: process.env.OWNER_ID,
+
+FIREBASE_CREDENTIALS: process.env.FIREBASE_CREDENTIALS
+
+};
