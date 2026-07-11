74e69bebf5dece52be8a40846f37c7aba1f7fc12
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2026-07-11 18:43:06 -0500

    Convert OWNER_ID to string in config

--- config.js
+++ config.js
@@ -3,11 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-
-BOT_TOKEN: process.env.BOT_TOKEN,
-
-OWNER_ID: process.env.OWNER_ID,
-
-FIREBASE_CREDENTIALS: process.env.FIREBASE_CREDENTIALS
-
+  BOT_TOKEN: process.env.BOT_TOKEN,
+  OWNER_ID: String(process.env.OWNER_ID),
+  FIREBASE_CREDENTIALS: process.env.FIREBASE_CREDENTIALS
 };
