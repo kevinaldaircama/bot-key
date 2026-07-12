0f3678456f8f0d120cadf9c9ad25463678a03d71
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2026-07-11 19:29:07 -0500

    Refactor Telegram bot initialization and imports

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 import TelegramBot from "node-telegram-bot-api";
 import config from "./config.js";
-import "./firebase.js";
 
 import registerStart from "./handlers/start.js";
 import registerRequest from "./handlers/request.js";
+import registerCallback from "./handlers/callback.js";
+import registerApprove from "./handlers/approve.js";
 
-const bot = new TelegramBot(config.BOT_TOKEN, {
-  polling: true
+
+const bot = new TelegramBot(config.BOT_TOKEN,{
+    polling:true
 });
 
 console.clear();
 
 console.log("=================================");
-console.log("🚀 MULTI SCRIPT VPN BOT");
+console.log("MULTI SCRIPT VPN BOT");
 console.log("=================================");
-console.log("✅ Bot iniciado correctamente");
 
 registerStart(bot);
 registerRequest(bot);
+registerCallback(bot);
+registerApprove(bot);
 
-bot.on("polling_error", (error) => {
-  console.log(error.message);
-});
+
+bot.on("polling_error",console.log);
