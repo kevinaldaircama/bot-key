710dc728205774ba5c7c85b74a463fde21434544
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2026-07-14 08:23:11 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,10 @@ dotenv.config();
 export default {
   BOT_TOKEN: process.env.BOT_TOKEN,
   OWNER_ID: String(process.env.OWNER_ID),
-  FIREBASE_CREDENTIALS: process.env.FIREBASE_CREDENTIALS
+  FIREBASE_CREDENTIALS: process.env.FIREBASE_CREDENTIALS,
+
+  CLOUDFLARE_TOKEN: process.env.CLOUDFLARE_TOKEN,
+  CLOUDFLARE_ZONE_ID: process.env.CLOUDFLARE_ZONE_ID,
+
+  DOMAIN: "socialstreaming.xyz"
 };
