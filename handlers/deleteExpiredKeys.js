533544813d8f4ff61a7bf55464b82dbad9b4c6f0
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2026-07-24 17:47:14 -0500

    Create deleteExpiredKeys.js

---
+++ handlers/deleteExpiredKeys.js
@@ -0,0 +1,40 @@
+import db from "../firebase.js";
+
+export default function deleteExpiredKeys() {
+
+    console.log("🗑️ Sistema de eliminación de keys iniciado.");
+
+    setInterval(async () => {
+
+        try {
+
+            const snap = await db.ref("keys").get();
+
+            if (!snap.exists()) return;
+
+            const now = Date.now();
+
+            for (const child of Object.entries(snap.val())) {
+
+                const key = child[0];
+                const data = child[1];
+
+                if (data.deleteAt && data.deleteAt <= now) {
+
+                    await db.ref(`keys/${key}`).remove();
+
+                    console.log(`🗑️ Key eliminada: ${key}`);
+
+                }
+
+            }
+
+        } catch (err) {
+
+            console.error(err);
+
+        }
+
+    }, 60000);
+
+              }
